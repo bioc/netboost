c54635ba9e21bc56c286f64f80bb3d1e17371a0c
Pascal Schlosser <pascal.schlosser@uniklinik-freiburg.de>
2021-01-29 20:29:56 +0100

    smaller example

--- R/data.R
+++ R/data.R
@@ -3,6 +3,6 @@
 #' chromosome 18 for 80 AML patients.
 #' @docType data
 #' @usage tcga_aml_meth_rna_chr18
-#' @format A data frame with 80 rows (patients) and 1000 variables (features).
+#' @format A data frame with 80 rows (patients) and 500 variables (features).
 #' @source \url{https://portal.gdc.cancer.gov/}
 NULL
